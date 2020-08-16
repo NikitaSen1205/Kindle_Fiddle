57e14d9baa3901fa1db6dc3e387674123e344b2e
NikitaSen1205 <50351884+NikitaSen1205@users.noreply.github.com>
2020-08-17 04:15:28 +0530

    init commit

---
+++ css/resp.js
@@ -0,0 +1,9 @@
+
+burger=document.querySelector('.burger')
+navbar=document.querySelector('.navbar')
+rightnav=document.querySelector('.rightnav')
+navlist=document.querySelector('.navlist')
+burger.addEventListener('click', ()=>)
+    rightnav.classList.toggle('visnav');
+    navlist.classList.toggle('visnav');
+    navbar.classList.toggle('heightnav');
